a85b687db7fb3d154ba3a75416e1dfc7c30c160a
Nikita Dudin <nikita.dudin@gmail.com>
2024-06-18 15:01:19 +0300

    Update example

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { Asset } from 'expo-asset';
-import { readAsStringAsync } from 'expo-file-system';
+import { EncodingType, readAsStringAsync } from 'expo-file-system';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
   useEffect(() => {
     (async () => {
-      const [asset] = Asset.loadAsync(require('./assets/icon.png'));
+      try {
+      const [asset] = await Asset.loadAsync(require('./assets/icon.png'));
 
-      await readAsStringAsync(asset.localUri); // error in iOS standalone app in iPhone XS and older versions. 
+      const b64 = await readAsStringAsync(asset.localUri, { encoding: EncodingType.Base64 }); // error in iOS standalone app on iPhone XS and older versions. 
+      setData(b64.slice(0, 64)); // iVBORw0KGgoAAAANSUhEUgAABAAAAAQA...
+      } catch (e) {
+        setError(e.message);
+      }
     })();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!data && !error && <Text>Reading asset data...</Text>}
+      {!!data && <Text>base64 data: {data}</Text>}
+      {!!error && <Text>error: {error}</Text>}
       <StatusBar style='auto' />
     </View>
   );
